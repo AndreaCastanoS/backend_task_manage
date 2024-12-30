f9623ed571dbbbf30208cedfbe2c82deb1c40c7f
AndreaCastanoS <andreacastsalazar@gmail.com>
2024-12-30 15:39:20 -0500

    Fix connection

--- config/database.js
+++ config/database.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
+
 const connection = async () => {
   try {
-    //
-    mongoose.connect(
-      process.env.DB,
-      {}
-    );
-    console.log("connected to dabase");
+    console.log("Database URI (from env):", process.env.DB); // Agrega este log
+    if (!process.env.DB) {
+      throw new Error("Missing DB environment variable");
+    }
+    await mongoose.connect(process.env.DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to database");
   } catch (err) {
-    console.log(err.message);
+    console.error("Database connection error:", err.message);
   }
 };
+
 connection();
